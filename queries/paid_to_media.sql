08229ebe10ae9f0fb5b2894695d5d18d94e0ad2a
John Cohoon <john.cohoon@wk.com>
2015-03-27 20:14:33 -0700

    fix multiple records with paid to media json

--- queries/paid_to_media.sql
+++ queries/paid_to_media.sql
@@ -6,10 +6,10 @@ from (
             ((n + interval '1 week'))::date as end_date
         from generate_series('2010-01-01'::date, '2015-01-01'::date, '1 week'::interval) n
     )
-    select sum(t.amount) as s, t.purpose_codes,t.book_type, i.end_date
+    select sum(t.amount) as s, i.end_date
     from working_transactions t
     right join intervals i
     on t.tran_date >= i.start_date and t.tran_date < i.end_date
     where purpose_codes ~~* any(array['%advertising%', '%postage%', '%yard sign%','%buttons%','%newspaper%','%literature%','%brochures%','%printing%'])
-    group by t.purpose_codes, i.end_date, t.book_type order by end_date
+    group by  i.end_date order by end_date
 ) t;
